610be894e026a5071a3cfaec0af99f281317cf8f
yuanzhiqiang <yuanzhiqiang@zhihu.com>
2021-12-07 13:13:34 +0800

    更新 automl

--- code/airserver/train/automl_ctl.py
+++ code/airserver/train/automl_ctl.py
@@ -1,6 +1,10 @@
+# coding:utf-8
+# Author: Zhiqiang Yuan
+
 import numpy as np
 from bayes_opt import BayesianOptimization
 import random
+
 
 def black_box_function(x, y):
     """Function with unknown internals we wish to maximize.
@@ -13,15 +17,105 @@ def black_box_function(x, y):
     return x ** 2 + y ** 2 + y
     # return random.random()
 
-if __name__=="__main__":
-    x = np.array([0.01*i for i in range(100)])
-    y = np.array([0.005*i for i in range(100)])
 
-    # z = black_box_function(x, y)
-    # print(z)
+# Grid Search
+def grid_search(pbounds, black_box_function, n_iter):
+    """
+    pbounds: params bound definition. dict { param: bound }
+        Example: {'x': (2, 4), 'y': (-3, 3)}
 
-    # Bounded region of parameter space
-    pbounds = {'x': (2, 4), 'y': (-3, 3)}
+    black_box_function: black box function definition.
+
+    n_iter: run n_iters. int
+        Note: n_iter must > len(pounds)
+
+    Returns: dict of results
+        Example: {'target': 22.0, 'params': {'x': 4.0, 'y': -3.0}}
+
+    """
+
+    n_params = len(pbounds) # n params
+
+
+# Random Search
+def random_search(pbounds, black_box_function, n_iter):
+    """
+    pbounds: params bound definition. dict { param: bound }
+        Example: {'x': (2, 4), 'y': (-3, 3)}
+
+    black_box_function: black box function definition.
+
+    n_iter: run n_iters. int
+        Note: n_iter must > len(pounds)
+
+    Returns:
+        max_value: dict of results
+            Example: {'target': 22.0, 'params': {'x': 4.0, 'y': -3.0}}
+        iter_value: dict of results
+            Example:    {
+            0: {'target': 11.101296174677724, 'params': {'x': 2.8340440094051482, 'y': 1.3219469606529488}},
+            1: {'target': 22.0, 'params': {'x': 4.0, 'y': -3.0}},
+            2: {'target': 15.279644924867352, 'params': {'x': 3.660003815774634, 'y': 0.9608275029525108}},
+            3: {'target': 18.096586513372948, 'params': {'x': 4.0, 'y': -2.031857210503951}},
+            4: {'target': 15.234250586825716, 'params': {'x': 3.0387909745202477, 'y': -3.0}},
+            5: {'target': 19.35692254600216, 'params': {'x': 3.88479676929217, 'y': -2.6249180236656837}}
+            }
+    """
+
+    # Params map
+    iter_value = {}
+    max_target = {'target': 1e-8}
+
+    for idx in range(n_iter):
+        # Generate random params
+        random_param = []
+        for k in pbounds.keys():
+            bound_start, bound_end = pbounds[k]
+            random_param.append(random.uniform(bound_start, bound_end))
+
+        # Calc target value
+        target = black_box_function(*random_param)
+
+        # Append to results
+        iter_value[idx] = {
+            'target': target,
+            'params': {k:v for k,v in zip(pbounds.keys(), random_param)}
+        }
+
+        # > max_target ?
+        if target > max_target['target']:
+            max_target = {
+            'target': target,
+            'params': {k:v for k,v in zip(pbounds.keys(), random_param)}
+        }
+
+    return max_target, iter_value
+
+
+#  Bayesian-optimization Search
+def bayesian_search(pbounds, black_box_function, n_iter):
+    """
+    pbounds: params bound definition. dict { param: bound }
+        Example: {'x': (2, 4), 'y': (-3, 3)}
+
+    black_box_function: black box function definition.
+
+    n_iter: run n_iters. int
+        Note: n_iter must > len(pounds)
+
+    Returns:
+        max_value: dict of results
+            Example: {'target': 22.0, 'params': {'x': 4.0, 'y': -3.0}}
+        iter_value: dict of results
+            Example:    {
+            0: {'target': 11.101296174677724, 'params': {'x': 2.8340440094051482, 'y': 1.3219469606529488}},
+            1: {'target': 22.0, 'params': {'x': 4.0, 'y': -3.0}},
+            2: {'target': 15.279644924867352, 'params': {'x': 3.660003815774634, 'y': 0.9608275029525108}},
+            3: {'target': 18.096586513372948, 'params': {'x': 4.0, 'y': -2.031857210503951}},
+            4: {'target': 15.234250586825716, 'params': {'x': 3.0387909745202477, 'y': -3.0}},
+            5: {'target': 19.35692254600216, 'params': {'x': 3.88479676929217, 'y': -2.6249180236656837}}
+            }
+    """
 
     optimizer = BayesianOptimization(
         f=black_box_function,
@@ -32,10 +126,27 @@ if __name__=="__main__":
 
     optimizer.maximize(
         init_points=0,
-        n_iter=5,
+        n_iter=n_iter,
     )
 
-    print(optimizer.max)
+    max_value = optimizer.max
 
-    for i, res in enumerate(optimizer.res):
-        print("Iteration {}: \n\t{}".format(i, res))
+    iter_value = {i: res for i, res in enumerate(optimizer.res)}
+
+    return max_value, iter_value
+
+
+if __name__ == "__main__":
+    x = np.array([0.01 * i for i in range(100)])
+    y = np.array([0.005 * i for i in range(100)])
+
+    # z = black_box_function(x, y)
+    # print(z)
+
+    # Bounded region of parameter space
+    pbounds = {'x': (2, 4), 'y': (-3, 3)}
+
+    max_value, iter_value = bayesian_search(pbounds, black_box_function, n_iter=5)
+
+    print(max_value)
+    print(iter_value)
